6e6aeeaec67c2500114ee66677513912471749be
mrteja2004 <119840454+mrteja2004@users.noreply.github.com>
2022-12-07 12:14:57 +0530

    #include<stdio.h> int main () {     int a;     scanf("%d",&a);     for(int i=1;i<=a;i++)     {         for(int j=1;j<=i;j++)         {             if(j==1||i==j||i==a)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,13 +3,21 @@ int main ()
 {
     int a;
     scanf("%d",&a);
-    for (int i=a;i>=1;i--)
+    for(int i=1;i<=a;i++)
     {
-        for (int j=1;j<=i;j++)
+        for(int j=1;j<=i;j++)
         {
-            printf("%d",j);
+            if(j==1||i==j||i==a)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
     }
+    
 }
